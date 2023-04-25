221f1cc701e2e5a801d02c39b92dea372a37a654
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2023-04-25 10:24:39 -0400

    Delete testP.java

--- Q2/testP.java
+++
@@ -1,9 +0,0 @@
-import Patient;
-public class testP {
-    public static void main(String[] args) {
-        Patient patientObj = new Patient("Juan", 18, "Male", "Critical");   //invoke constructor
-        System.out.println(patientObj.getCondition());  //test the get
-        patientObj.setCondition("Stable");  //test the set
-        System.out.println(patientObj.getCondition());
-    }
-}
