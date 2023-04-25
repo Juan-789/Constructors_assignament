654674cf153a8fce356c27253b3f5ec506ac327e
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2023-04-25 19:53:11 -0400

    Update Main.java

--- Q2/Main.java
+++ Q2/Main.java
@@ -2,19 +2,19 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Patient patientObj1 = new Patient("Juan", 18, "Male", "Critical");   //invoke constructor1
-      // System.out.println(patientObj1.getName());
-        String credentials1 = patientObj1.getName()+", "+patientObj1.getAge()+" year old "+patientObj1.getGender()+" with "+ patientObj1.getCondition()+" condition.";
         Patient patientObj2  = new Patient("Evan", 17, "Male", "Stable"); 
-        String credentials2 =  patientObj2.getName()+", "+patientObj2.getAge()+" year old "+patientObj2.getGender()+" with "+ patientObj2.getCondition()+" condition.";
+        
         // Add a office administrator interface so that any number of patients’ information can be stored in the system,
         // and any patient’s information can be accessed and displayed one at a time.
         Scanner inputObj = new Scanner(System.in);
         System.out.println("What patient do you want to know records of? ");
-        String input = inputObj.nextLine();
-        if (input=="1") {
-            System.out.println(credentials1);
+        int input = inputObj.nextInt();
+
+        if (input==1) {
+            System.out.println(patientObj1.getName()+", "+patientObj1.getAge()+" year old "+patientObj1.getGender()+" with "+ patientObj1.getCondition()+" condition.");
         }
-        else if (input=="2") {
-            System.out.println(credentials2);
+        else if (input==2) {
+            System.out.println(patientObj2.getName()+", "+patientObj2.getAge()+" year old "+patientObj2.getGender()+" with "+ patientObj2.getCondition()+" condition.");
         }
     }
+}
